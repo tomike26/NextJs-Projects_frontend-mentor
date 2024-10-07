6ce2a80085d2ceec42644338988cf6eb6191a09d
tomike26 <kolajotomike26@gmail.com>
2024-10-07 09:44:56 +0100

    faq_accordion_fixed

---
+++ faq-accordion/app/page.tsx
@@ -0,0 +1,44 @@
+"use client"
+import { useState } from "react";
+import faqs from "../util/faqs";
+
+export default function Home() {
+  const [expanded, setExpanded] = useState<number | null>(null);
+
+  const handleClick = (index: number) => {
+    setExpanded(expanded === index ? null : index);
+  };
+
+  return (
+    <div className="faq-accordions">
+      <div className="faq-section">
+        <div className="faq-header">
+          <img src="./images/icon-star.svg" />
+          <h1>FAQs</h1>
+        </div>
+        {faqs.map((faq: any, index: number) => (
+          <div className="faq-questions" key={index}>
+            <div
+              className={`faq-wrapper ${expanded === index ? "active" : ""}`}
+              onClick={() => handleClick(index)}
+            >
+              <p>{faq.question}</p>
+              <img
+                src={
+                  expanded === index
+                    ? "./images/icon-minus.svg"
+                    : "./images/icon-plus.svg"
+                }
+                alt={expanded === index ? "Collapse" : "Expand"}
+              />
+            </div>
+
+            <div className={`faq-answer ${expanded === index ? "active" : ""}`}>
+              <p>{faq.answer}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
